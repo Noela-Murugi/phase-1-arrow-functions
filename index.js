b47e3102efb9caf4eabad162035608ecf1aa5223
Noela-Murugi <murugin254@gmail.com>
2022-08-16 14:53:50 +0300

    completed the assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+//divide function
+function divide(){
+    return 2000 / 100;
+}
+
+//square function
+const square = num1 => num1 * num1;
+square(2);
+
+//add function
+const add = (param1, param2) => param1 + param2;
+add(31,29);
